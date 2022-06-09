bd94ed452b378a04b02b8dbb700a56df789df7f1
kevinrt0999 <73867014+kevinrt0999@users.noreply.github.com>
2022-06-09 00:40:05 -0600

    Clients Sign Up SP
    We created the SP that sign up a new client, including the validation if his/her identification is an existing one or not.

--- DataBases/User.sql
+++ DataBases/User.sql
@@ -3,11 +3,52 @@ CREATE DATABASE User; -- MYSQL
 USE User;
 
 CREATE TABLE UserData(
-	Id INT PRIMARY KEY NOT NULL auto_increment,
-	Name VARCHAR(50) NOT NULL,
+	Id INT NOT NULL AUTO_INCREMENT,
+	NameC VARCHAR(50) NOT NULL,
 	Adress VARCHAR(50) NOT NULL,
 	Identification VARCHAR(50) NOT NULL,
 	Phone VARCHAR(50) NOT NULL,
-	Email VARCHAR(50) NOT NULL
+	Email VARCHAR(50) NOT NULL,
+	PRIMARY KEY(Id),
+	UNIQUE(Identification)
 ); 
 
+use user;
+-- drop procedure InsertClient;
+DELIMITER //
+CREATE PROCEDURE InsertClient(
+	IN in_name VARCHAR(50),
+    IN in_adress VARCHAR(50), 
+    IN in_identification VARCHAR(50), 
+    IN in_phone VARCHAR(50), 
+    IN in_email VARCHAR(50))
+BEGIN
+
+    DROP TABLE IF EXISTS temporal;
+	CREATE TEMPORARY TABLE temporal(
+      in_name2 VARCHAR(50), 
+      in_adress2 VARCHAR(50),
+      in_id2 VARCHAR(50),
+      in_phone2 VARCHAR(50),
+      in_email2 VARCHAR(50),
+      var2 INT
+    );
+    SET @tmp_id = 'NULL';
+    SET @exist = 0;
+
+	SET @tmp_id = (select Identification FROM UserData WHERE Identification = in_identification);
+    SET @exist = (SELECT ISNULL(@tmp_id));
+    -- SELECT @exist;
+	INSERT INTO temporal(in_name2, in_adress2, in_id2, in_phone2, in_email2, var2) VALUES(in_name, in_adress, in_identification, in_phone, in_email, @exist);
+
+	INSERT INTO UserData(NameC, Adress, Identification, Phone, Email) 
+	SELECT in_name2, in_adress2, in_id2, in_phone2, in_email2
+	FROM temporal
+	WHERE var2 = 1;
+
+	SELECT @exist;
+
+END//
+
+	
+DELIMITER ;
